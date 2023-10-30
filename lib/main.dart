eaaaa611882c6a199844d4073d54de8e539a3d10
Pierre Juarez U. <76144339+pierre-juarez@users.noreply.github.com>
2023-10-29 23:56:10 -0500

    [add]: Welcome Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/feature/welcome/pages/welcome_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,14 +11,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Whatsapp Me',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text("Demo page"),
-        ),
-      ),
+      theme: ThemeData.dark(),
+      home: const WelcomePage(),
     );
   }
 }
